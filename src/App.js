272d61485ea8e642330cd1de1ab488397a8d1fa5
Matt Haffner <mrhaffner13@gmail.com>
2020-11-01 19:20:15 -0600

    created a function to make an object with the relevant data from the fetched JSON. The object will now be stored in a weather state, and that will be set with useEffect.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
 const App = () => {
+
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    getWeather()
+  });
+
+
+
+  const createWeather = (data) => {
+    return {
+      temp: data.main.temp,
+      temp_max: data.main.temp_max,
+      temp_min: data.main.temp_min,
+      humidity: data.main.humidity,
+      wind_speed: data.wind.speed,
+      description: data.weather[0].description,
+    }
+  }
 
   const getWeather = async () => {
     const response = await fetch(`//api.openweathermap.org/data/2.5/weather?q=London&appid=ed2da20037c4b8ee5881791f4fe6aa0d`, {mode: 'cors'})
     const data = await response.json()
-    console.log(data)
+    setWeather(createWeather(data))
+    
   }
 
-  getWeather()
+  function log () {
+    console.log(weather)
+  } 
 
   return (
     <div>
       <h1>Hi</h1>
+      <button onClick={log}>Get Weather</button>
     </div>
   );
 }
