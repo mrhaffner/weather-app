25d03d75ea1f53c8baa1b1eb23cce664237938f5
Matt Haffner <mrhaffner13@gmail.com>
2020-11-01 18:24:12 -0600

    updated readme and created a function to fetch data from the weather API and log it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 const App = () => {
+
+  const getWeather = async () => {
+    const response = await fetch(`//api.openweathermap.org/data/2.5/weather?q=London&appid=ed2da20037c4b8ee5881791f4fe6aa0d`, {mode: 'cors'})
+    const data = await response.json()
+    console.log(data)
+  }
+
+  getWeather()
+
   return (
     <div>
       <h1>Hi</h1>
